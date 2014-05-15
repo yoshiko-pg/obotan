68d07e66bc59104071fa4b993d82e435abc7a699
maasa <maasa.giss2@gmail.com>
2014-05-15 09:53:00 +0900

    user create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'static_page#index'
   get 'static_page/index'
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
