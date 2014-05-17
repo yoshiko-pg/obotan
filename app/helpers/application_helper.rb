95b23d25da7f8e1c32e908d58cf581236da361ed
maasa <maasa.giss2@gmail.com>
2014-05-17 10:43:14 +0900

    編集削除追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
   def get_words
     @words = Word.where(user_id: @user).order(:category_id, :created_at)
   end
+
+  def twitter_icon user_name, size = 'normal'
+    "http://furyu.nazo.cc/twicon/#{user_name}/#{size}"
+  end
 end
