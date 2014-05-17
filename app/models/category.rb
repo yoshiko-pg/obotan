61e001739b88c8801f916ca1ad25f8e4f00b1b5c
maasa <maasa.giss2@gmail.com>
2014-05-17 12:37:01 +0900

    validates追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
   belongs_to :user
   has_many :words, dependent: :destroy
+
+  validates :user_id, presence: true
+  validates :name, presence: true, length: { maximum: 40 }, 
+            uniqueness: { case_sensitive: false }
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,11 @@
 class Word < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+
+  validates :user_id, presence: true
+  validates :category_id, presence: true
+  validates :word, presence: true, length: { maximum: 100 },
+            uniqueness: { case_sensitive: false }
+  validates :mean, presence: true, length: { maximum: 100 },
+            uniqueness: { case_sensitive: false }
 end
