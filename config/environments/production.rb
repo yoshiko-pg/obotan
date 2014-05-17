4cbfc6fb7b540d91be6ad1edf95fbbdfe19d30e9
maasa <maasa.giss2@gmail.com>
2014-05-17 12:28:22 +0900

    for heroku assets path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,10 @@ Rails.application.configure do
   end
 
 
+  config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+  # Precompile additional assets
+  config.assets.precompile += %w( .svg .eot .woff .ttf .otf )
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -31,7 +35,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -85,8 +89,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
-  config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
-  # Precompile additional assets
-  config.assets.precompile += %w( .svg .eot .woff .ttf .otf )
 end
