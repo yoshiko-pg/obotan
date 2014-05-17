8ba5661869546859c1e933773e1c99dd69f8a058
maasa <maasa.giss2@gmail.com>
2014-05-17 12:08:21 +0900

    production環境にもTwitterキー設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  Devise.setup do |config|
+    config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_SECRET_KEY'], :display => 'popup'
+  end
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
